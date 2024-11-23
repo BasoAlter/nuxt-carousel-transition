6f1da0d6223aa9824d405cc5a620156827cc1ee9
BasoAlter <baso.alter@gmail.com>
2024-11-23 12:45:59 +0000

    temporary fix for autoplay

--- pages/index.vue
+++ pages/index.vue
@@ -5,12 +5,17 @@ const items = [
   'https://picsum.photos/640/640?random=2',
   'https://picsum.photos/640/640?random=3',
 ]
+const isMounted = ref(false)
+
+onMounted(() => {
+  isMounted.value = true
+})
 </script>
 
 <template>
   <main>
     <ULink to="/other">other page</ULink>
-    <UCarousel v-slot="{ item }" :items="items" dots loop autoplay>
+    <UCarousel v-slot="{ item }" :items="items" dots loop :autoplay="isMounted">
       <img :src="item" width="320" height="320" class="rounded-lg" />
     </UCarousel>
   </main>
